0060fb85756d560898f348b452eeb089901dec18
Damin <min_0625@kakao.com>
2025-07-03 16:21:06 +0900

    [⚙ FEATURE] String 형태인 loginUser 호출을 LoginUser로 변경

--- src/main/LoginManager.java
+++ src/main/LoginManager.java
@@ -1,7 +1,10 @@
 package main;
 
 import dao.EmployeeService;
+import dao.LoginLog;
+import model.LoginUser;
 
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 // 사용자 로그인 (메인)
@@ -17,13 +20,20 @@ public class LoginManager {
 
             // 로그인 여부 확인하는 dao.EmployeeService 객체 생성
             EmployeeService employeeService = new EmployeeService();
-            // login(id, pw) 호출 및 결과 확인
-            String LoginUserName = employeeService.loginCheck(id, pw);
+            LoginUser loginUser = employeeService.loginCheck(id, pw);
 
             // 로그인 성공 여부에 따른 메세지 출력
-            if(LoginUserName != null) {
+            if(loginUser != null) {
                 System.out.println("로그인 성공!");
-                System.out.printf("사원 %s 님, 안녕하세요", LoginUserName);
+                System.out.printf("사원 %s 님, 안녕하세요", loginUser.getUserName());
+                // 로그인 성공 시 사원 정보 (id, name, 로그인 date, time 전달)
+                // 1. loginuser 객체 생성 <- 로그인 정보 + loginTime + emp_id 저장
+
+
+                LocalDateTime currentDateTime = LocalDateTime.now();
+                LoginLog loginLog = new LoginLog();
+//                loginLog.SaveLoginLog(, currentDateTime);
+                // 2. 호출 -> DB에 출근 기록 저장 + log_id 변환 -> loginUser 저장
                 break;
             }
             else {
