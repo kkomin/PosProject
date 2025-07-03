1d99f30c22a0785779746a0ee3b160557ccd686b
Damin <min_0625@kakao.com>
2025-07-03 14:19:39 +0900

    [🛠 LITTLE_FIX] sql 데이터 스키마명 변경

--- src/EmployeeService.java
+++ src/EmployeeService.java
@@ -9,20 +9,20 @@ public class EmployeeService {
     Connection connection = ConnectionDB.getConnectionDB();
 
     // 쿼리 작성 employees의 id -> ?, pw -> ?
-    // select * from employees where emp_id = ? and emp_pw = ?;
+    // select * from employees where user_id = ? and user_pw = ?;
     private final String sql = """
-            SELECT * FROM EMPLOYEES WHERE EMP_ID = ? AND EMP_PW = ?
+            SELECT * FROM EMPLOYEES WHERE USER_ID = ? AND USER_PW = ?
             """;
     public EmployeeService() throws SQLException {
         System.out.println("DB 연결 오류 발생");
     }
 
-    public boolean loginCheck(int empId, String empPw) {
+    public boolean loginCheck(String userId, String userPw) {
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             // PreparedStatement로 파라미터 바인딩
-            preparedStatement.setInt(1, empId);
-            preparedStatement.setString(2, empPw);
+            preparedStatement.setString(1, userId);
+            preparedStatement.setString(2, userPw);
             // ResultSet의 결과 확인
             final ResultSet resultSet = preparedStatement.executeQuery();
             // 결과로 존재 여부 확인
