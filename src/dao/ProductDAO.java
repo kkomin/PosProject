5f50febf43f0e3bd75d6d7d795951fac00fd17d3
Damin <min_0625@kakao.com>
2025-07-04 15:02:56 +0900

    [⚙ FEATURE] 파라미터 바인딩 구현

--- src/dao/ProductDAO.java
+++ src/dao/ProductDAO.java
@@ -3,8 +3,8 @@ package dao;
 import db.ConnectionDB;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 // 등록하는 제품 -> DB에 추가 (insert)
@@ -12,9 +12,9 @@ public class ProductDAO {
     private Connection connection;
 
     // 제품 등록 sql
-    private final String insertProduct = """
+    public final String insertProduct = """
             INSERT INTO PRODUCTS(PROD_ID, PROD_NAME, COMPANY, EXPIRATION, IS_ADULT, PRICE, STOCK)
-            VALUES (prod_id_seq.nextval, ?, ?, ?, ?, ?, ?, ?)
+            VALUES (prod_id_seq.nextval, ?, ?, ?, ?, ?, ?)
             """;
 
     // 등록한 제품 prod_id 받아오는 sql
@@ -28,14 +28,26 @@ public class ProductDAO {
         }
     }
 
-    public void RegisterProduct(String name, String company, String expiration, String isAdult, int price, int stock) {
+    public void RegisterProduct(String name, String company, Date expiration, Boolean isAdult, int price, int stock) {
         PreparedStatement preparedStatement;
         try {
             preparedStatement = connection.prepareStatement(insertProduct);
             // 파라미터 바인딩
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, company);
+            preparedStatement.setDate(3, expiration);
+            preparedStatement.setBoolean(4, isAdult);
+            preparedStatement.setInt(5, price);
+            preparedStatement.setInt(6, stock);
 
-            preparedStatement.executeUpdate();
+            int result = preparedStatement.executeUpdate();
 
+            if(result > 0) {
+                System.out.println("제품 등록 완료!\n");
+            }
+            else {
+                System.out.println("제품 등록 실패...\n");
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
