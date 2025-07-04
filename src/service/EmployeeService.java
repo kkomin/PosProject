ec50db2644da377d8c5acbe8afd5ae735ab5051d
Damin <min_0625@kakao.com>
2025-07-04 13:51:30 +0900

    [⚙ FEATURE] 메뉴 1. 제품 이동 메서드 호출

--- src/service/EmployeeService.java
+++ src/service/EmployeeService.java
@@ -19,6 +19,33 @@ public class EmployeeService {
             int input = 0;
             try {
                 input = sc.nextInt();
+                switch (input) {
+                    case 1 :
+                        // 제품 등록 메서드 호출
+                        ProductRegister.registerProduct();
+                        break;
+                    case 2:
+                        // 재고 확인 메서드 호출
+                        break;
+                    case 3:
+                        // 제품 검색 메서드 호출
+                        break;
+                    case 4:
+                        // 입고 처리 메서드 호출
+                        break;
+                    case 5:
+                        // 결제 메서드 호출
+                        break;
+                    case 6 :
+                        // 매출 조회 메서드 호출
+                        break;
+                    case 0:
+                        // 퇴근 (일급 계산) 메서드 호출
+                        break;
+                    default:
+                        System.out.println("잘못 입력하셨습니다.");
+                        continue;
+                }
             } catch (Exception e) {
                 System.out.println("숫자를 입력하세요\n");
             }
