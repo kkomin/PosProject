2509c64be69f029aca54b8b28a73891ca270407a
Damin <min_0625@kakao.com>
2025-07-04 15:03:14 +0900

    [⚙ FEATURE] getter 설정

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,18 +1,20 @@
 package model;
 
+import java.sql.Date;
+
 // 제품 정보 객체
 public class Product {
     private int prodId;
     private String prodName;
     private String company;
-    private String expiration;
+    private Date expiration;
     private boolean isAdult;
     private int price;
     private int stock;
 
 
     // 제품 정보 등록 생성자
-    public Product(String prodName, String company, String expiration, boolean isAdult, int price, int stock) {
+    public Product(String prodName, String company, Date expiration, boolean isAdult, int price, int stock) {
         this.prodName = prodName;
         this.company = company;
         this.expiration = expiration;
@@ -20,4 +22,29 @@ public class Product {
         this.price = price;
         this.stock = stock;
     }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public Date getExpiration() {
+        return expiration;
+    }
+
+    public boolean isAdult() {
+        return isAdult;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
 }
