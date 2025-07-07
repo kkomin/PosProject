de0d551b84ce7c5cc97720d01b86cf84cdcc264c
user <min_0625@kakao.com>
2025-07-07 21:50:15 +0900

    [📄 DOCUMENT] 결제 파일 로직 주석으로 구현

--- src/service/PaymentService.java
+++ src/service/PaymentService.java
@@ -2,4 +2,13 @@ package service;
 
 // 결제 방식 처리 (카드 or. 현금 구분, 잔고 갱신)
 public class PaymentService {
+    private final int INITIAL = 1234000;
+
+    // 결제 처리
+    
+    // 결제 방법 선택
+    
+    // 카드 결제 처리
+    
+    // 현금 결제 처리
 }
