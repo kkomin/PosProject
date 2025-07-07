3002773d12baefd00ac4f2c08e08d58247ccefcc
Damin <min_0625@kakao.com>
2025-07-07 10:07:34 +0900

    [⚙ FEATURE] PRODUCTS 전체 목록 조회 SQL 작성

--- src/dao/InventoryDAO.java
+++ src/dao/InventoryDAO.java
@@ -1,4 +1,16 @@
 package dao;
 
+import java.sql.Connection;
+
 public class InventoryDAO {
+    private final Connection connection;
+
+    // 모든 제품 조회
+    private final String selectSql = """
+            SELECT * FROM PRODUCTS
+            """;
+
+    public InventoryDAO(Connection connection) {
+        this.connection = connection;
+    }
 }
