5685807a185f2b246a246812ca8c50891a01f704
Damin <min_0625@kakao.com>
2025-07-07 10:08:01 +0900

    [⚙ FEATURE] DB 연결

--- src/dao/InventoryDAO.java
+++ src/dao/InventoryDAO.java
@@ -1,9 +1,12 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class InventoryDAO {
-    private final Connection connection;
+    private Connection connection;
 
     // 모든 제품 조회
     private final String selectSql = """
@@ -12,5 +15,14 @@ public class InventoryDAO {
 
     public InventoryDAO(Connection connection) {
         this.connection = connection;
+    }
+
+    public void inventory() {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(selectSql)){
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+        } catch (SQLException e) {
+            System.out.println("INVENTORY SQL 구문 오류" + e.getMessage());
+        }
     }
 }
