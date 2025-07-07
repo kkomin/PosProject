6d7495173ec64d254b2128c3407062b9e78086e0
Damin <min_0625@kakao.com>
2025-07-07 14:51:34 +0900

    [⚙ FEATURE] 일급 계산 로직 구현

--- src/service/WorkWageService.java
+++ src/service/WorkWageService.java
@@ -6,25 +6,26 @@ import model.LoginUser;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class WorkWageService {
     public void logout(LoginUser user) {
-        // 현재 시간
         try (Connection connection = ConnectionDB.getConnectionDB()) {
             WorkLogDAO workLogDAO = new WorkLogDAO(connection);
 
             // 해당 사원의 가장 최근 login_log 조회
-            LocalDateTime currentLogout = LocalDateTime.now();
 
-            // 로그인 시간 가져오기
-
+            // 계산에 필요한 정보 가져오기
+            int id = user.getEmpId();       // emp_id(PK) 가져오기
+            LocalDateTime loginTime = user.getLoginTime();  // 로그인한 시간
+            LocalDateTime currentLogout = LocalDateTime.now();  // 현재 시간
+            int wage = user.getHourlyWage();    // 시급 조회
 
             // 근무 시간 분 단위로 계산
-
-            // 시급 조회
-
             // 일급 계산 = 시급 / 60 * 근무시간(분)
+            long workMinutes = Duration.between(loginTime, currentLogout).toMinutes();   // 일한 시간
+            long dailyWage = wage / 60 * workMinutes;
 
             // worklogdao에 업데이트
 
