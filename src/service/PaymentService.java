c4e87c367f5aaf21ab7ca301b2d92c2536761c29
user <min_0625@kakao.com>
2025-07-07 21:52:48 +0900

    [⚙ FEATURE] 결제 방법 선택 기능 틀 구현

--- src/service/PaymentService.java
+++ src/service/PaymentService.java
@@ -1,10 +1,30 @@
 package service;
 
+import java.util.Scanner;
+
 // 결제 방식 처리 (카드 or. 현금 구분, 잔고 갱신)
 public class PaymentService {
     private final int INITIAL = 1234000;
 
     // 결제 처리
+    public void proccessPay(int INITIAL) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("결제 방법 (카드 : 1 / 현금 : 2)");
+        System.out.print("선택 : ");
+        int method = sc.nextInt();
+        sc.nextLine();
+
+        switch(method) {
+            case 1 :
+                // 카드 선택
+                break;
+            case 2:
+                // 현금 선택
+                break;
+            default:
+                System.out.println("잘못된 결제 방법 입니다.\n");
+        }
+    }
     
     // 결제 방법 선택
     
