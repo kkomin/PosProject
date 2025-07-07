8d4236c466410b659206c6aaa851bc5e9cbb7874
Damin <min_0625@kakao.com>
2025-07-07 13:07:47 +0900

    [⚙ FEATURE] 카테고리에 따른 검색 메서드 생성

--- src/dao/SearchProductDAO.java
+++ src/dao/SearchProductDAO.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 // 제품 검색 -> NAME, COMPANY
@@ -13,30 +14,59 @@ public class SearchProductDAO {
     }
 
     // 제품명으로 검색
-    public void SearchName() {
+    public void SearchName(String name) {
         String searchNameSql = """
             SELECT * FROM PRODUCTS WHERE PROD_NAME LIKE ?
             """;
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(searchNameSql)) {
             // 파라미터 바인딩
-            preparedStatement.executeQuery();
+            preparedStatement.setString(1, "%" + name + "%");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            boolean found = false;
+
+            while(resultSet.next()) {
+                found = true;
+                String prodName = resultSet.getString("PROD_NAME");
+                String prodCompany = resultSet.getString("COMPANY");
+                int price = resultSet.getInt("PRICE");
+                int stock = resultSet.getInt("STOCK");
+                System.out.printf("제품명 : %s | 회사 %s | 가격 : %,d | 수량 : %d\n", prodName, prodCompany, price, stock);
+            }
+            if(!found) {
+                System.out.println("검색 결과가 존재하지 않습니다.\n");
+            }
+
         } catch (SQLException e) {
-            System.out.println("SEARCH NAME SELECT 오류" + e.getMessage());
+            System.out.println("SEARCH NAME SELECT 오류\n" + e.getMessage());
         }
     }
 
     // 회사명으로 검색
-    public void SearchCompany() {
+    public void SearchCompany(String company) {
         String searchCompanySql = """
                 SELECT * FROM PRODUCTS WHERE COMPANY LIKE ?
                 """;
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(searchCompanySql)){
             // 파라미터 바인딩
-            preparedStatement.executeQuery();
+            preparedStatement.setString(1, "%" + company + "%");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            boolean found = false;
+
+            while(resultSet.next()) {
+                found = true;
+                String prodName = resultSet.getString("PROD_NAME");
+                String prodCompany = resultSet.getString("COMPANY");
+                int price = resultSet.getInt("PRICE");
+                int stock = resultSet.getInt("STOCK");
+                System.out.printf("제품명 : %s | 회사 %s | 가격 : %,d | 수량 : %d\n", prodName, prodCompany, price, stock);
+            }
+            if(!found) {
+                System.out.println("검색 결과가 존재하지 않습니다.\n");
+            }
         } catch (SQLException e) {
-            System.out.println("SEARCH COMPANY SELECT 오류" + e.getMessage());
+            System.out.println("SEARCH COMPANY SELECT 오류\n" + e.getMessage());
         }
 
     }
