c1feefda0b4abdc23c9f7f472b44712d6c4aab2b
Damin <min_0625@kakao.com>
2025-07-07 10:57:39 +0900

    [⚙ FEATURE] 입고 수량 추가 및 기능 과정 주석처리

--- src/service/InventoryService.java
+++ src/service/InventoryService.java
@@ -24,6 +24,14 @@ public class InventoryService {
             Product selected = products.get(index);
 
             // 입고 처리 (재고 증가 + inventory_logs 추가)
+            // 입고 수량 1 ~ 10 사이 랜덤하게
+            int quantity = random.nextInt(10) + 1;
+
+            // 현재 날짜도 포함해서 입고 기록 추가
+
+            // PRODUCT의 STOCK도 수량만큼 증가
+
+            // 입고한 정보 출력
         }
         catch (SQLException e) {
             System.out.println("Inventory 연결 오류" + e.getMessage());
