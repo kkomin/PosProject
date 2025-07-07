7d4d95c4f0062032267ff041280595c8950ba246
Damin <min_0625@kakao.com>
2025-07-07 15:39:30 +0900

    [🐛 BUG_FIXED] SQL 구문 오류 수정 및 파라미터 바인딩 수정

--- src/dao/WorkLogDAO.java
+++ src/dao/WorkLogDAO.java
@@ -6,7 +6,7 @@ import java.sql.*;
 import java.time.LocalDateTime;
 
 public class WorkLogDAO {
-    private Connection connection;
+    private final Connection connection;
 
     public WorkLogDAO(Connection connection) {
         this.connection = connection;
@@ -15,7 +15,7 @@ public class WorkLogDAO {
     // 해당 EMPLOYEE가 가장 마지막에 로그인한 기록 가져오기
     public LoginUser LastLoginLog(int empId) {
         String lastLogSql = """
-                SELECT l.log_id, l.login_time, l.logout_time,e.user_name, e.user_id, e.hourly_wage
+                SELECT l.log_id, l.emp_id, l.login_time, l.logout_time,e.user_name, e.user_id, e.hourly_wage
                    FROM login_logs l
                    JOIN employees e ON l.emp_id = e.emp_id
                    WHERE l.emp_id = ?
@@ -51,10 +51,10 @@ public class WorkLogDAO {
                 """;
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(logoutLog)){
-            preparedStatement.setInt(1, logId);
-            preparedStatement.setDate(2, Date.valueOf(logoutTime.toLocalDate()));
-            preparedStatement.setLong(3, workMinutes);
-            preparedStatement.setInt(4, dailyWage);
+            preparedStatement.setDate(1, Date.valueOf(logoutTime.toLocalDate()));
+            preparedStatement.setLong(2, workMinutes);
+            preparedStatement.setInt(3, dailyWage);
+            preparedStatement.setInt(4, logId);
             preparedStatement.executeUpdate();
 
         } catch (SQLException e) {
