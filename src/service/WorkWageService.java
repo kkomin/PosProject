aafaf93cf60b26879b9439da9943eb8ba67d2162
Damin <min_0625@kakao.com>
2025-07-07 14:39:04 +0900

    [⚙ FEATURE] dao 파일 호출 및 로그인 정보 가져오기 위한 매개변수 설정

--- src/service/WorkWageService.java
+++ src/service/WorkWageService.java
@@ -1,21 +1,36 @@
 package service;
 
+import dao.WorkLogDAO;
+import db.ConnectionDB;
+import model.LoginUser;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
 public class WorkWageService {
-    public void logout(int empId) {
+    public void logout(LoginUser user) {
         // 현재 시간
+        try (Connection connection = ConnectionDB.getConnectionDB()) {
+            WorkLogDAO workLogDAO = new WorkLogDAO(connection);
 
-        // 해당 사원의 가장 최근 login_log 조회
+            // 해당 사원의 가장 최근 login_log 조회
+            LocalDateTime currentLogout = LocalDateTime.now();
 
-        // 로그인 시간 가져오기
+            // 로그인 시간 가져오기
 
-        // 근무 시간 분 단위로 계산
 
-        // 시급 조회
+            // 근무 시간 분 단위로 계산
 
-        // 일급 계산 = 시급 / 60 * 근무시간(분)
+            // 시급 조회
 
-        // worklogdao에 업데이트
+            // 일급 계산 = 시급 / 60 * 근무시간(분)
 
-        // 콘솔에 일급 출력
+            // worklogdao에 업데이트
+
+            // 콘솔에 일급 출력
+        } catch (SQLException e) {
+            System.out.println("시급 계산 connection 오류" + e.getMessage());
+        }
     }
 }
