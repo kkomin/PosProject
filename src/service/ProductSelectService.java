3e1ce4cc58719401fe7acb52931336061ab683be
user <min_0625@kakao.com>
2025-07-07 20:53:49 +0900

    [📄 DOCUMENT] 사용자 주문 처리 로직 주석으로 구현

--- src/service/ProductSelectService.java
+++ src/service/ProductSelectService.java
@@ -1,6 +1,26 @@
 package service;
 
+import java.util.Scanner;
+
 // 사용자 주문 처리
 public class ProductSelectService {
+    public void selectProduct() {
+        Scanner sc = new Scanner(System.in);
 
+        // 제품 목록 출력
+
+        // 사용자로부터 제품 선택 받기
+
+        // 선택한 제품 정보 가져오기
+
+        // 유통기한 확인
+
+        // 성인 인증 체크
+
+        // 수량 입력
+
+        // 가격 계산
+
+        // 재고 차감
+    }
 }
