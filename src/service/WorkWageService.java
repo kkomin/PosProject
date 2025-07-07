19752576dd967b2560051d3e22a7757533f56003
Damin <min_0625@kakao.com>
2025-07-07 14:27:40 +0900

    [⚙ FEATURE] 기능 로직 과정 주석 처리

--- src/service/WorkWageService.java
+++ src/service/WorkWageService.java
@@ -1,7 +1,21 @@
 package service;
 
 public class WorkWageService {
-    public void wageEmployee() {
+    public void logout(int empId) {
+        // 현재 시간
 
+        // 해당 사원의 가장 최근 login_log 조회
+
+        // 로그인 시간 가져오기
+
+        // 근무 시간 분 단위로 계산
+
+        // 시급 조회
+
+        // 일급 계산 = 시급 / 60 * 근무시간(분)
+
+        // worklogdao에 업데이트
+
+        // 콘솔에 일급 출력
     }
 }
