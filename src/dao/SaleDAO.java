e6e879b94d59f581a1ce106da39b3b0cb8c510e8
Damin <min_0625@kakao.com>
2025-07-11 15:09:21 +0900

    [🆕 NEW] 판매 아이템 테이블에 추가하기 위한 DAO 파일 생성

---
+++ src/dao/SaleDAO.java
@@ -0,0 +1,21 @@
+package dao;
+
+import java.sql.Connection;
+
+public class SaleDAO {
+    private final Connection connection;
+
+    public SaleDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    // SALE 테이블에 insert
+    public void insertSale() {
+
+    }
+
+    // sale_item 테이블에 insert
+    public void insertSaleItem() {
+
+    }
+}
