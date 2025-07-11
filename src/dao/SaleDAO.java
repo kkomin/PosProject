ee6fdd2d63b24deca115de999c2d8bfbedc5e67a
Damin <min_0625@kakao.com>
2025-07-11 16:05:20 +0900

    [⚙ FEATURE] SALE 테이블에 INSERT 해주는 로직 구현

--- src/dao/SaleDAO.java
+++ src/dao/SaleDAO.java
@@ -12,13 +12,19 @@ public class SaleDAO {
     }
 
     // SALE 테이블에 insert
-    public void insertSale() {
+    public void insertSale(int empId, int totalPrice, String paymentType, Character isAdult) {
     String insertSql = """
             INSERT INTO SALES(SALE_ID, SALE_DATE, EMP_ID, TOTAL_PRICE, PAYMENT_tYPE, ADULT_CHECK)
             VALUES(SALE_SEQ.NEXTVAL, SYSDATE, ?, ?, ?, ?)
             """;
         try (PreparedStatement preparedStatement = connection.prepareStatement(insertSql)){
             // 파라미터 바인딩
+            preparedStatement.setInt(1, empId);
+            preparedStatement.setInt(2, totalPrice);
+            preparedStatement.setString(3, paymentType);
+            preparedStatement.setString(4, String.valueOf(isAdult));
+
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("sale insert 오류 발생");
         }
